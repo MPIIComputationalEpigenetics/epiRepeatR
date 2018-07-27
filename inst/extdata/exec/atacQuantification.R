a1d91e8efd8502ec5523eb9b71e79a886638574a
Fabian Müller <fmueller@mpi-inf.mpg.de>
2018-07-26 17:34:01 -0700

    …

--- inst/extdata/exec/atacQuantification.R
+++ inst/extdata/exec/atacQuantification.R
@@ -4,8 +4,8 @@ ap <- ArgumentParser()
 ap$add_argument("-i", "--in", action="store", dest="input", help="Input file of aligned ATAC-seq reads (bam)")
 ap$add_argument("-o", "--out", action="store", dest="output", help="Output file (rds)")
 ap$add_argument("-c", "--config", action="store", help="Config file (json)")
-ap$add_argument("-g", "--genome", action="store_true", dest="genomeRepTrack", default=FALSE, help="Enables parsing from genome methylation calls.")
-ap$add_argument("--grt", action="store", dest="genomeRepTrack", help="Enables parsing from genome methylation calls. Specifies Path to an RDS file containing a GenomeRepeatTrack object.")
+ap$add_argument("-g", "--genome", action="store_true", dest="doGenomeRepTrack", default=FALSE, help="Enables parsing from genome methylation calls.")
+ap$add_argument("--grt", action="store", dest="genomeRepTrack", help="Specifies Path to an RDS file containing a GenomeRepeatTrack object.")
 cmdArgs <- ap$parse_args()
 logger.cmd.args(cmdArgs)
 
@@ -21,7 +21,7 @@ if (METHOD=="genomeScale"){
 		abund <- epiRepeatR:::getRepeatGenomeCovg(grt)
 	}
 }
-if (cmdArgs$genomeRepTrack){
+if (cmdArgs$doGenomeRepTrack){
 	logger.info("Quantify enrichment from genome alignment")
 	ga.atac <- readRDS(cmdArgs$input)
 	quantObj <- epiRepeatR:::normCounts(ga.atac, method=METHOD, abund=abund)
